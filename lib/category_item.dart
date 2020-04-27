37500bdc3b6dbe8ec512b2c38269691b5e209415
VanHungLe0611 <hungvipthaibinh@gmail.com>
2020-04-27 18:23:07 +0200

    add foods and route to a new food page

---
+++ lib/category_item.dart
@@ -0,0 +1,42 @@
+import 'package:FoodApp/category_meals_screen.dart';
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String title;
+  final Color color;
+  final String id;
+
+  //CategoryItem({@required this.title,@required this.color});
+
+  CategoryItem(this.id, this.title, this.color);
+
+  void selectCategory(BuildContext context) {
+    Navigator.of(context).pushNamed(CategoryMealsScreen.routeName, arguments: {
+      'id': id,
+      'title': title,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      onTap: () => selectCategory(context),
+      child: Container(
+        padding: EdgeInsets.all(15),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [color.withOpacity(0.7), color],
+              begin: Alignment.bottomLeft,
+              end: Alignment.bottomRight),
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
+    );
+  }
+}
